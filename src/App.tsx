431402f101713b8715cc01b088ad1b17702b52c5
revanderground <vnthuanx@gmail.com>
2023-06-28 18:28:08 +0200

    feat: :sparkles: App and MessagesDisplay

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import CodeDisplay from "./components/CodeDisplay"
+import MessagesDisplay from "./components/MessagesDisplay"
+
 const App = () => {
   return (
     <div className="app">
-      
+      <MessagesDisplay />
+      <input />
+      <CodeDisplay />
+      <div className="button-container">
+        <button id="get-query">Get Query!</button>
+        <button id="clear-chat">Clear Chat</button>
+      </div>
     </div>
   )
 }
--- src/components/MessagesDisplay.tsx
+++ src/components/MessagesDisplay.tsx
@@ -1,7 +1,9 @@
+import MessageDisplay from "./MessageDisplay"
+
 const MessagesDisplay = () => {
     return (
       <div className="messages-display">
-        
+        <MessageDisplay />
       </div>
     )
   }
