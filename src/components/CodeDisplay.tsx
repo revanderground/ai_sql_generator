5532a60d6698b4a3771696f61fa4d817a878db27
revanderground <vnthuanx@gmail.com>
2023-06-29 09:33:38 +0200

    style: :lipstick: Style Code display and buttons

--- src/components/CodeDisplay.tsx
+++ src/components/CodeDisplay.tsx
@@ -1,7 +1,14 @@
 const CodeDisplay = () => {
     return (
       <div className="code-display">
-        
+        <div className="buttons">
+          <div className="button first"></div>
+          <div className="button middle"></div>
+          <div className="button last"></div>
+          <div className="code-output">
+            <p></p>
+          </div>
+        </div>
       </div>
     )
   }
